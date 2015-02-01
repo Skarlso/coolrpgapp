ca0e0b4422f9ca96c88c8f7bf87ec8553d12cc65
Skarlso <skarlso777@gmail.com>
2015-02-01 18:09:10 +0000

    Trying to add a simple redirect.

--- cool_rpg_app/cool_rpg_app.js
+++ cool_rpg_app/cool_rpg_app.js
@@ -1,4 +1,39 @@
+Usernames = new Mongo.Collection("usernames");
+
 if (Meteor.isClient) {
+  Router.route('/', function () {
+  // render the Home template with a custom data context
+    this.render('adduser', {data: {username: "bka"}});
+  });
+  Router.route('/story', function () {
+    this.render('Story');
+  });
+/*
+  Template.body.events({
+    "submit .new-user": function (event) {
+      // This function is called when the new task form is submitted
+
+      var username = event.target.username.value;
+
+      Usernames.insert({
+        username: username,
+        createdAt: new Date() // current time
+      });
+
+
+
+      // Clear form
+      event.target.username.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });*/
+  Template.body.events({
+  'click #clickme': function () {
+    Router.go('/story');
+  }
+  });
 }
 
 if (Meteor.isServer) {
